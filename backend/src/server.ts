e08a701c5f596b94c0c0074639f95f25acb3b85f
anand-106 <anands2003106@gmail.com>
2025-09-02 19:21:52 +0530

    ADDED price polller and Backend

---
+++ backend/src/server.ts
@@ -0,0 +1,81 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+
+const PORT = 4000;
+const JWT_SECRET = "anand";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json("hello world");
+});
+
+app.post("/api/v1/signup", (req, res) => {
+  const { email } = req.body;
+
+  const token = jwt.sign({ email }, JWT_SECRET);
+
+  const LINK = `http://localhost:3000/auth/verify?token=${token}`;
+
+  try {
+    sgMail.send({
+      from: "anands2003106@gmail.com",
+      to: email,
+      subject: "magiclink",
+      html: `<a href=${LINK} > MagicLink</a>`,
+    });
+    console.log("email send");
+    res.json({ message: "email sent" });
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.post("/api/v1/signin", async (req, res) => {
+  const { email } = req.body;
+
+  const token = jwt.sign({ email }, JWT_SECRET);
+
+  const LINK = `http://localhost:3000/auth/verify?token=${token}`;
+
+  try {
+    await sgMail.send({
+      from: "anands2003106@gmail.com",
+      to: email,
+      subject: "magiclink",
+      html: `<a href=${LINK} > MagicLink</a>`,
+    });
+    console.log("email send");
+    res.json({ message: "email sent" });
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.get("/api/v1/verify", (req, res) => {
+  const { token } = req.query;
+
+  const { email } = jwt.verify(token as string, JWT_SECRET) as any;
+
+  if (!email) res.status(401).json({ message: "Invalid token" });
+
+  res.setHeader("Set-Cookie", `token=${token}; Max-Age=3600`); // Cookie expires in 1 hour
+  res.send("Cookie set successfully");
+
+  //   res.cookie("token", token);
+  //   res.json({ email });
+});
+
+app.listen(PORT, () => {
+  console.log("app running on port ", PORT);
+});
