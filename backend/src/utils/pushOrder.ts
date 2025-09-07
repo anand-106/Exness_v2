bebf52131a17095dd19e4cdfb480ca91b7de6274
anand-106 <anands2003106@gmail.com>
2025-09-07 23:37:46 +0530

    check point

---
+++ backend/src/utils/pushOrder.ts
@@ -0,0 +1,19 @@
+import { Queue } from "bullmq";
+import { Redis } from "ioredis";
+import { OrderQueue } from "../types/types";
+import { getReturnOrderId } from "./getReturnOrderId";
+
+export const client = new Redis();
+
+export async function pushOrder(id:string,data: any) {
+  try {
+    await client.xadd("create-order", "*", "id",id,"type","order","data", JSON.stringify(data));
+    console.log(`order  queued`);
+    const resId = await getReturnOrderId(data)
+    return resId
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+
